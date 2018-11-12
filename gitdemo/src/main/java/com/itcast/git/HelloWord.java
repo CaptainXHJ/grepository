69975f4d3d6ff14022c341d30ec511c4896950a5
xhj <915722189@qq.com>
2018-11-12 22:14:24 +0800

    master分支

--- gitdemo/src/main/java/com/itcast/git/HelloWord.java
+++ gitdemo/src/main/java/com/itcast/git/HelloWord.java
@@ -4,5 +4,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word");
         System.out.println("65436124865684");
+        System.out.println("master分支");
     }
 }
