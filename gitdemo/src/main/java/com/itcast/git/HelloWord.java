3b3d18bfb8066f8a916e8e5eff677f5a0b57f5bf
xhj <915722189@qq.com>
2018-11-12 21:51:32 +0800

    初始化

--- gitdemo/src/main/java/com/itcast/git/HelloWord.java
+++ gitdemo/src/main/java/com/itcast/git/HelloWord.java
@@ -3,5 +3,6 @@ package com.itcast.git;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("Hello Word");
+        System.out.println("65436124865684");
     }
 }
